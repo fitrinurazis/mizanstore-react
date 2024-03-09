c40608037754c6c6a8be4935fc14446b43be9b97
Fitri Nur Azis <110329611+fitrinurazis@users.noreply.github.com>
2024-03-09 20:05:15 +0700

    fix bug

--- src/components/Book Card/BookCard.jsx
+++ src/components/Book Card/BookCard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 const BookCard = ({ id, name, image, author, price, discon }) => {
   const shortName = name.length > 20 ? name.substring(0, 20) + "..." : name;
+  const discountedPrice = price - price * (discon / 100);
 
   return (
     <li key={id} className="snap-start group">
@@ -19,18 +20,23 @@ const BookCard = ({ id, name, image, author, price, discon }) => {
         </div>
         <div className="w-32 px-1 lg:w-36 ">
           <h3 className="text-xs font-extrabold uppercase whitespace-pre-line">
-            {shortName}{" "}
+            {shortName}
           </h3>
           <p className="text-[11px] font-normal py-1">{author}</p>
           <div className="pt-2">
             <span className="text-xs font-semibold line-through">
-              Rp {price}{" "}
+              Rp {price}
             </span>
+
             <span className="p-1 text-xs font-normal text-white bg-red-600 rounded-md">
               {discon}%
             </span>
           </div>
-          <p className="py-1 text-sm font-bold text-red-600">Rp 68.000</p>
+
+          <p className="py-1 text-sm font-bold text-red-600">
+            Rp {discountedPrice.toFixed(3)}
+          </p>
+
           <p className="w-5 h-5"></p>
         </div>
       </div>
